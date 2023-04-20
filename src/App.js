84b3364cf42f54b4709338fbf553206b53aabb5d
giovyc93 <118946227+giovyc93@users.noreply.github.com>
2023-04-20 14:48:32 +0200

    nuovo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import React from "react";
+import PokemonList from "./PokemonList";
+import axios from "axios";
 
 function App() {
+  const [pokemon, setPokemon] = useState(["bulbasaur", "charmender"])
+  axios.get("https://pokeapi.co/api/v2/pokemon/").then(res => {
+    setPokemon(res.data.results.map(p=>p.name))
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <PokemonList pokemon={pokemon} />
     </div>
   );
 }
